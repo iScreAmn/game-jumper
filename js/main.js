3a708007339bcb61ebd8082cc87db577e591745c
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-02-14 04:13:09 +0400

    Updated character size

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+document.fonts.ready.then(drawStartScreen);
+
 
 // СОЗДАНИЕ CANVAS И КОНТЕКСТА 
-
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
@@ -11,10 +12,9 @@ canvas.height = 500;
 
 
 // ЗАГРУЗКА ИЗОБРАЖЕНИЙ
-
-const horseImg = new Image();
-horseImg.src = "./img/person.webp";
-horseImg.onload = () => console.log("Horse loaded!");
+const characterImg = new Image();
+characterImg.src = "./img/person.webp";
+characterImg.onload = () => console.log("Character loaded!");
 
 const obstacleImg = new Image();
 obstacleImg.src = "./img/obstracle.png";
@@ -22,15 +22,14 @@ obstacleImg.onload = () => console.log("Obstacle loaded!");
 
 
 // СОЗДАНИЕ ПЕРЕМЕННЫХ
-
-let horse = {
+let character = {
   x: 50,
   y: 280,
-  width: 60,
-  height: 60,
+  width: 80,
+  height: 80,
   dy: 0,
   gravity: 0.5,
-  jumpPower: -10,
+  jumpPower: -12,
   onGround: true,
 };
 
@@ -39,45 +38,45 @@ let gameSpeed = 4;
 let score = 0;
 let gameStarted = false;
 
-// ОТРИСОВКА ЛОШАДИ
-function drawHorse() {
-  ctx.drawImage(horseImg, horse.x, horse.y, horse.width, horse.height);
+// ОТРИСОВКА ПЕРСОНАЖА
+function drawCharacter() {
+  ctx.drawImage(characterImg, character.x, character.y, character.width, character.height);
 }
 
 // ОТРИСОВКА СТАРТОВОГО ЭКРАНА
 
 function drawStartScreen() {
-  ctx.fillStyle = "black";
-  ctx.font = "40px Arial";
+  ctx.fillStyle = "#fff";
+  ctx.font = "80px 'Pixelify Sans', serif";
   ctx.textAlign = "center";
-  ctx.fillText("START - Space Button", canvas.width / 2, canvas.height / 2);
+  ctx.fillText("START", canvas.width / 2, canvas.height / 2 + 50);
 }
 
 // ПРЫЖОК
 
 function jump() {
-  if (horse.onGround) {
-    horse.dy = horse.jumpPower;
-    horse.onGround = false;
+  if (character.onGround) {
+    character.dy = character.jumpPower;
+    character.onGround = false;
   }
 }
 
 // ОБНОВЛЕНИЕ ПЕРСОНАЖА (ГРАВИТАЦИЯ)
 
-function updateHorse() {
-  horse.y += horse.dy;
-  horse.dy += horse.gravity;
+function updateCharacter() {
+  character.y += character.dy;
+  character.dy += character.gravity;
 
-  if (horse.y >= 280) {
-    horse.y = 280;
-    horse.onGround = true;
+  if (character.y >= 280) {
+    character.y = 280;
+    character.onGround = true;
   }
 }
 
 // ГЕНЕРАЦИЯ ПРЕПЯТСТВИЙ
 
 function generateObstacle() {
-  obstacles.push({ x: canvas.width, y: 300, width: 20, height: 30 });
+  obstacles.push({ x: canvas.width, y: 300, width: 40, height: 50 });
 }
 
 // ОБНОВЛЕНИЕ ПРЕПЯТСТВИЙ
@@ -104,10 +103,10 @@ function updateObstacles() {
 
   // ПРОВЕРКА СТОЛКНОВЕНИЯ  
     if (
-      horse.x < obstacles[i].x + obstacles[i].width &&
-      horse.x + horse.width > obstacles[i].x &&
-      horse.y < obstacles[i].y + obstacles[i].height &&
-      horse.y + horse.height > obstacles[i].y
+      character.x < obstacles[i].x + obstacles[i].width &&
+      character.x + character.width > obstacles[i].x &&
+      character.y < obstacles[i].y + obstacles[i].height &&
+      character.y + character.height > obstacles[i].y
     ) {
       // alert("Game Over! Score: " + score);
       location.reload();
@@ -132,8 +131,8 @@ document.addEventListener("keydown", (e) => {
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawHorse();
-  updateHorse();
+  drawCharacter();
+  updateCharacter();
   updateObstacles();
   requestAnimationFrame(gameLoop);
 }
