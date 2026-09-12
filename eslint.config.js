4c0ced9011cba13ad62ff2330b1abb5316579aa7
Dimitri. J <jmukhadze.dimitri@gmail.com>
2026-09-12 12:36:42 +0400

    Add ESLint and Vitest

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'prefer-const': 'error',
+      eqeqeq: ['error', 'always'],
+    },
+  },
+];
